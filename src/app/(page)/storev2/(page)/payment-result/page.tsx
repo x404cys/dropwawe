570f347ef99dbb5a78edd5a802a0ea2f87665871
x404cys <0xabdulrahmanmanaf@gmail.com>
2025-11-10 19:31:42 +0300

    H4p

--- src/app/(page)/storev2/(page)/payment-result/page.tsx
+++ src/app/(page)/storev2/(page)/payment-result/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { CheckCircle2, XCircle, ArrowRight, Home } from 'lucide-react';
