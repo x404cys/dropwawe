6566e2323bbe4b6b4dd8217d6ae445c140e7778f
x404cys <0xabdulrahmanmanaf@gmail.com>
2025-11-14 23:50:23 +0300

    H144520

--- src/app/api/storev2/payment/callback/route.ts
+++ src/app/api/storev2/payment/callback/route.ts
@@ -1,14 +1,33 @@
 import { NextResponse } from 'next/server';
-
+import { prisma } from '@/app/lib/db';
 export async function GET(req: Request) {
   const params = new URL(req.url).searchParams;
   const tranRef = params.get('tranRef') ?? '';
   const respStatus = params.get('respStatus') ?? '';
   const respMessage = params.get('respMessage') ?? '';
   const cartId = params.get('cartId') ?? '';
+  const customerEmail = params.get('customerEmail') ?? '';
+  const signature = params.get('signature') ?? '';
+  const token = params.get('token') ?? '';
+  const acquirerRRN = params.get('acquirerRRN') ?? '';
+  const payment = await prisma.paymentOrder.create({
+    data: {
+      tranRef: tranRef,
+      respCode: respStatus,
+      respMessage: respMessage,
+      cartId: cartId,
+      amount: 100,
+      customerEmail: customerEmail,
+      signature: signature,
+      token: token,
+      status: respStatus === 'A' ? 'Success' : 'Failed',
+      order: {
+        connect: { id: 'cmex85j990001js04dyve39bc' },
+      },
+    },
+  });
 
   const returnUrl = `${new URL(req.url).origin}/storev2/payment-result?tranRef=${tranRef}&respStatus=${respStatus}&respMessage=${respMessage}&cartId=${cartId}`;
-  console.log('PAYTABS CALLBACK GET:', { tranRef, respStatus, respMessage, cartId });
 
   return NextResponse.redirect(returnUrl, { status: 303 });
 }
@@ -21,7 +40,6 @@ export async function POST(req: Request) {
   }
 
   const returnUrl = `${new URL(req.url).origin}/storev2/payment-result?tranRef=${data.tranRef}&respStatus=${data.respStatus}&respMessage=${data.respMessage}&cartId=${data.cartId}`;
-  console.log('PAYTABS CALLBACK POST:', data);
 
   return NextResponse.redirect(returnUrl, { status: 303 });
 }
