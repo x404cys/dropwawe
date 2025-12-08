9a4028d82e2eca25cec460e93e97c0c82a2a342a
ayman <ayman@gmail.com>
2025-12-08 18:23:41 +0300

    Hi broe31230wwwwwwwwwwwwwwwwwwwwww

--- src/app/api/dashboard/setting/update/[theme]/route.ts
+++ src/app/api/dashboard/setting/update/[theme]/route.ts
@@ -15,7 +15,7 @@ export async function POST(
     if (!session || !session.user?.id) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
-
+//
     const userId = session.user.id;
 
     const store = await prisma.store.update({
