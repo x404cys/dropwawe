d9ee027b8dc4eed0816ed469b348b0a9c204f0d2
ayman <ayman@gmail.com>
2025-12-09 00:44:10 +0300

    add supplier

--- src/app/(page)/login/page.tsx
+++ src/app/(page)/login/page.tsx
@@ -16,7 +16,7 @@ export default function SignInPage() {
   const handleGoogleSignIn = async () => {
     setIsLoading(true);
     await signIn('google', {
-      callbackUrl: '/Dashboard/create-store/Supplier',
+      callbackUrl: '/Dashboard/create-store',
     });
 
     setIsLoading(false);
