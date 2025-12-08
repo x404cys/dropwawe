0e038cc2a9a1a474d3d0040033cec5b715a4f493
ayman <ayman@gmail.com>
2025-12-08 05:16:42 +0300

    Hi broe31230

--- src/app/api/dashboard/setting/update/[theme]/route.ts
+++ src/app/api/dashboard/setting/update/[theme]/route.ts
@@ -3,14 +3,14 @@ import { getServerSession } from 'next-auth';
 import { prisma } from '@/app/lib/db';
 import { authOperation } from '@/app/lib/authOperation';
 
-export async function POST(
-  req: Request,
-  context: { params: Promise<{ theme: 'NORMAL' | 'MODERN' }> }
-) {
+export async function POST(req: Request, context: { params: { theme: string } }) {
   try {
     const session = await getServerSession(authOperation);
+    const { theme } = context.params;
 
-    const { theme } = await context.params;
+    if (theme !== 'NORMAL' && theme !== 'MODERN') {
+      return NextResponse.json({ error: 'Invalid theme' }, { status: 400 });
+    }
 
     if (!session || !session.user?.id) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
@@ -20,9 +20,7 @@ export async function POST(
 
     const store = await prisma.store.update({
       where: { userId },
-      data: {
-        theme: theme,
-      },
+      data: { theme },
     });
 
     return NextResponse.json({
