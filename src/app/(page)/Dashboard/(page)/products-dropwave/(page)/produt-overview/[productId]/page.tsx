8fb11f4233efbba9f32f9d54956769b29a1caf2d
404cys <0xabdulrahmanmanaf@gmail.com>
2026-02-06 16:55:55 +0300

    v1.2.0

--- src/app/(page)/Dashboard/(page)/products-dropwave/(page)/produt-overview/[productId]/page.tsx
+++ src/app/(page)/Dashboard/(page)/products-dropwave/(page)/produt-overview/[productId]/page.tsx
@@ -377,22 +377,22 @@ export default function ProductPage() {
                   //   return;
                   // }
 
-                  const customProduct = {
-                    ...product,
-                    selectedColor,
-                    selectedSize,
-                    colors: product.colors?.filter(c => c.hex === selectedColor) || [],
-                    sizes: product.sizes?.filter(s => s.size === selectedSize) || [],
-                    shippingPrice: product.user?.Store?.[0]?.shippingPrice?.toString() ?? '',
-                  };
+                  // const customProduct = {
+                  //   ...product,
+                  //   selectedColor,
+                  //   selectedSize,
+                  //   colors: product.colors?.filter(c => c.hex === selectedColor) || [],
+                  //   sizes: product.sizes?.filter(s => s.size === selectedSize) || [],
+                  //   shippingPrice: product.user?.Store?.[0]?.shippingPrice?.toString() ?? '',
+                  // };
 
-                  addToCartWithQtyByKey(
-                    customProduct,
-                    quantity,
-                    // `cart/${store?.id}`,
-                    selectedColor,
-                    selectedSize
-                  );
+                  // addToCartWithQtyByKey(
+                  //   customProduct,
+                  //   quantity,
+                  //   // `cart/${store?.id}`,
+                  //   selectedColor,
+                  //   selectedSize
+                  // );
                   toast.success(`تم اضافة ${quantity} الى السلة بنجاح`);
                 }}
                 className="hover-scale h-14 flex-1 cursor-pointer rounded-lg border"
