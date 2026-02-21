a3e3984e0a6b5e07fe4d92311c5f742461001f32
404cys <0xabdulrahmanmanaf@gmail.com>
2026-02-21 16:46:33 +0300

    lounch

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import { getServerSession } from 'next-auth';
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
@@ -16,6 +17,10 @@ export async function middleware(req: NextRequest) {
   const session = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
+    cookieName:
+      process.env.NODE_ENV === 'production'
+        ? '__Secure-next-auth.session-token'
+        : 'next-auth.session-token',
   });
   const role = session?.role as string | undefined;
 
